a7d59e81647a16a65b8d3e70087eef4098acd08a
CMScofield7 <scoundreldays2@gmail.com>
2023-03-02 18:30:08 -0300

    Requisitos 7, 8 e 9 Concluídos!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+// import Header from './components/Header';
 
 function App() {
   return (
     <Switch>
-      {/* Adicionar rotas aqui */}
-      <Route exact path="/" />
+      <Switch>
+        <Route path="/meals/:id/in-progress" />
+        <Route path="/drinks/:id/in-progress" />
+        <Route path="/meals/:id" component={ Recipes } />
+        <Route path="/drinks/:id" component={ Recipes } />
+        <Route path="/meals" component={ Recipes } />
+        <Route path="/drinks" component={ Recipes } />
+        <Route path="/profile" />
+        <Route path="/done-recipes" />
+        <Route path="/favorite-recipes" />
+        <Route exact path="/" />
+      </Switch>
     </Switch>
   );
 }
