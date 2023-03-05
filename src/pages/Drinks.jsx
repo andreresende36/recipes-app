c559993950cf3d4786d8259298c60174689c61ee
Bruno Alvim Duarte <brunoalvimduarte@gmail.com>
2023-03-05 16:03:25 -0300

    requisitos 21, 22, 23 prontos

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -6,14 +6,19 @@ import Footer from '../components/Footer';
 import CategoryButton from '../components/CategoryButton';
 
 function Drinks(props) {
-  const { drinks, drinksCategories } = useContext(RecipesContext);
+  const { drinks, drinksCategories, setApiURLDrinks } = useContext(RecipesContext);
   const numberOfCategoriesToShow = 5;
   return (
     <div className="drinks-page">
       <div className="categories-buttons-container">
         {drinksCategories.slice(0, numberOfCategoriesToShow).map(({ strCategory }) => (
-          <CategoryButton key={ strCategory } strCategory={ strCategory } />
+          <CategoryButton
+            key={ strCategory }
+            strCategory={ strCategory }
+            drinkOrMeal="drink"
+          />
         ))}
+        <button data-testid="All-category-filter" onClick={ () => setApiURLDrinks('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=') }>Remove All Filters</button>
       </div>
       <Header
         headerType={ { title: 'Drinks', profileIcon: true, searchIcon: true } }
