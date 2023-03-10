6762af429bf020109b586f6791ab46d911c63ad3
andreresende36 <andreresende36@gmail.com>
2023-03-10 17:02:05 -0300

    testes Meals e Drinks

---
+++ src/tests/Drinks.test.js
@@ -0,0 +1,52 @@
+import { screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+import HeaderProvider from '../context/HeaderProvider';
+
+const firstCardName = '0-card-name';
+jest.setTimeout(20000);
+
+describe('Teste do componente Drinks.jsx', () => {
+  it('Teste do botão de remover todos os filtros', async () => {
+    const { history } = renderWithRouter(
+      <HeaderProvider>
+        <App />
+      </HeaderProvider>,
+    );
+    act(() => {
+      history.push('/drinks');
+    });
+    await waitFor(() => {
+      expect(screen.getByTestId('Cocktail-category-filter')).toBeInTheDocument();
+      expect(screen.getByTestId(firstCardName)).toBeInTheDocument();
+    });
+    const initialArrayRecipeNames = [];
+    const cocktailArrayRecipeNames = [];
+    const finalArrayRecipeNames = [];
+    for (let i = 0; i <= 11; i += 1) {
+      const recipeName = screen.getByTestId(`${i}-card-name`);
+      initialArrayRecipeNames.push(recipeName.textContent);
+    }
+    userEvent.click(screen.getByTestId('Cocktail-category-filter'));
+    await waitFor(() => {
+      expect(screen.getByTestId(firstCardName).textContent).toBe('155 Belmont');
+    });
+    for (let i = 0; i <= 11; i += 1) {
+      const recipeName = screen.getByTestId(`${i}-card-name`);
+      cocktailArrayRecipeNames.push(recipeName.textContent);
+    }
+    expect(initialArrayRecipeNames).not.toEqual(cocktailArrayRecipeNames);
+    userEvent.click(screen.getByTestId('All-category-filter'));
+    await waitFor(() => {
+      expect(screen.getByTestId(firstCardName).textContent).toBe('GG');
+    }, { timeout: 20000 });
+    for (let i = 0; i <= 11; i += 1) {
+      const recipeName = screen.getByTestId(`${i}-card-name`);
+      finalArrayRecipeNames.push(recipeName.textContent);
+    }
+    expect(initialArrayRecipeNames).toEqual(finalArrayRecipeNames);
+  });
+});
---
+++ src/tests/RecipeCard.test.js
@@ -0,0 +1,44 @@
+import { screen, waitFor } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import HeaderProvider from '../context/HeaderProvider';
+import renderWithRouter from './helpers/renderWithRouter';
+
+jest.setTimeout(20000);
+const firstButton = '0-recipe-button';
+
+describe('Teste do componente RecipeCard', () => {
+  it('Meals - Testa se ao clicar no botão de receita é redirecionado corretamente', async () => {
+    const { history } = renderWithRouter(
+      <HeaderProvider>
+        <App />
+      </HeaderProvider>,
+    );
+    act(() => {
+      history.push('/meals');
+    });
+    await waitFor(() => {
+      expect(screen.getByTestId(firstButton)).toBeInTheDocument();
+    });
+    userEvent.click(screen.getByTestId(firstButton));
+    expect(history.location.pathname).toBe('/meals/52977');
+  });
+
+  it('Drinks - Testa se ao clicar no botão de receita é redirecionado corretamente', async () => {
+    const { history } = renderWithRouter(
+      <HeaderProvider>
+        <App />
+      </HeaderProvider>,
+    );
+    act(() => {
+      history.push('/drinks');
+    });
+    await waitFor(() => {
+      expect(screen.getByTestId(firstButton)).toBeInTheDocument();
+    });
+    userEvent.click(screen.getByTestId(firstButton));
+    expect(history.location.pathname).toBe('/drinks/15997');
+  });
+});
