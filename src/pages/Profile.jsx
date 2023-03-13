4b1b838a688a9c6a58ed1140d14cc2a3ea188b00
max fukui <max.fukui.dev@gmail.com>
2023-03-13 14:24:17 -0400

    colocando novas informacoes de css

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import '../styles/profile.css';
+import { ReactComponent as ProfileLogo } from '../images/Perfil.svg';
+import { ReactComponent as OkVector } from '../images/okVector.svg';
 // 57 - Implemente os elementos da tela de perfil respeitando os atributos descritos no protótipo Ok
 // 58 - Implemente a solução de maneira que o e-mail da pessoa usuária deve estar visível
 
@@ -30,21 +33,40 @@ function Profile(props) {
         headerType={ { title: 'Profile', profileIcon: true, searcIcon: false } }
         { ...props }
       />
-      <p data-testid="profile-email">{email}</p>
-      <button
-        data-testid="profile-done-btn"
-        onClick={ () => history.push('/done-recipes') }
-      >
-        Done Recipes
+      <div className="tela-profile">
+        <div className="user-info">
+          <ProfileLogo className="logo" />
+          <p data-testid="profile-email" className="email">{email}</p>
+        </div>
+        <div className="button-profile">
+          <OkVector />
+          <button
+            data-testid="profile-done-btn"
+            onClick={ () => history.push('/done-recipes') }
+          >
+            Done Recipes
 
-      </button>
-      <button
-        data-testid="profile-favorite-btn"
-        onClick={ () => history.push('/favorite-recipes') }
-      >
-        Favorite Recipes
-      </button>
-      <button data-testid="profile-logout-btn" onClick={ handleLogout }>Logout</button>
+          </button>
+        </div>
+        <div className="button-profile">
+          <OkVector />
+          <button
+            data-testid="profile-favorite-btn"
+            onClick={ () => history.push('/favorite-recipes') }
+          >
+            Favorite Recipes
+          </button>
+        </div>
+        <div className="button-profile">
+          <OkVector />
+          <button
+            data-testid="profile-logout-btn"
+            onClick={ handleLogout }
+          >
+            Logout
+          </button>
+        </div>
+      </div>
       <Footer { ...props } />
 
     </div>
