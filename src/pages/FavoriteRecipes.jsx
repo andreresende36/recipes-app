dbffff9776816b2657c839c3b37f248976f51773
nome completo <email@email.com>
2023-03-16 09:49:08 -0300

    removendo sobras pós pull da branch main

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -49,78 +49,16 @@ function FavoriteRecipes(props) {
         headerType={ { title: 'Favorite Recipes', profileIcon: true, searchIcon: false } }
         { ...props }
       />
-      { filteredRecipes.map((recipe, index) => (
-        <div key={ recipe.id }>
-          <Link
-            to={ recipe.type === 'meal'
-              ? `/meals/${recipe.id}` : `/drinks/${recipe.id}` }
-          >
-            <h2 data-testid={ `${index}-horizontal-name` }>{ recipe.name }</h2>
-            <img
-              data-testid={ `${index}-horizontal-image` }
-              src={ recipe.image }
-              alt=""
-              className="favorite-recipe-img"
-            />
-          </Link>
-          { recipe.alcoholicOrNot ? (
-            <p data-testid={ `${index}-horizontal-top-text` }>
-              { recipe.alcoholicOrNot }
-              {' '}
-              -
-              {' '}
-              { recipe.category}
-            </p>
-          ) : (
-            <p data-testid={ `${index}-horizontal-top-text` }>
-              { recipe.nationality }
-              {' '}
-              -
-              {' '}
-              { recipe.category }
-            </p>
-          )}
-          <button
-            onClick={ () => {
-              // handleClickFavorite();
-              const localStorageRecipe = JSON.parse(localStorage
-                .getItem('favoriteRecipes'))
-                .filter((favRecipe) => favRecipe.id !== recipe.id);
-              localStorage.setItem('favoriteRecipes', JSON.stringify(localStorageRecipe));
-              setRecipes(localStorageRecipe);
-            } }
-          >
-            <img
-              src={ blackHeartIcon }
-              alt="favorite-icon"
-              data-testid={ `${index}-horizontal-favorite-btn` }
-            />
-          </button>
-          <button
-            onClick={ () => {
-              copy(`http://localhost:3000/meals/${recipe.id}`);
-              setIsLinkCopied(true);
-            } }
-          >
-            <img
-              data-testid={ `${index}-horizontal-share-btn` }
-              src={ shareSvg }
-              alt="share-icon"
-            />
-          </button>
-          {isLinkCopied && <p>Link copied!</p>}
-        </div>
-      ))}
-      <button
-        data-testid="filter-by-meal-btn"
-        type="button"
-        onClick={ () => setFilter('meal') }
-        className="tag-name"
-      >
-        Meals
-
-      </button>
       <div className="filters-container">
+        <button
+          data-testid="filter-by-meal-btn"
+          type="button"
+          onClick={ () => setFilter('meal') }
+          className="tag-name"
+        >
+          Meals
+
+        </button>
         <button
           data-testid="filter-by-drink-btn"
           type="button"
